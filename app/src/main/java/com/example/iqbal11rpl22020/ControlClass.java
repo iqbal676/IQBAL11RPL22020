dffe0c560a59abc4bfa3a99b51efd4b951a878af
ASUS <irfan10rpl22019@gmail.com>
2020-08-25 00:24:08 -0700

    Initial commit

---
+++ app/src/main/java/com/example/iqbal11rpl22020/ControlClass.java
@@ -0,0 +1,26 @@
+package com.example.iqbal11rpl22020;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ControlClass extends AppCompatActivity {
+    SharedPreferences sharedPreferences;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        sharedPreferences=getSharedPreferences("login", Context.MODE_PRIVATE);
+
+        if (sharedPreferences.getString("nama","").isEmpty()){
+            startActivity(new Intent(ControlClass.this,MainActivity.class));
+            finish();
+        }else{
+            startActivity(new Intent(ControlClass.this,menuActivity.class));
+            finish();
+        }
+    }
+}
